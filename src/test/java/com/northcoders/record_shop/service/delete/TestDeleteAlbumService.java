a973409bd7216066d20ef211338966569a685db4
dMarsh <125702046+Alan-Turing13@users.noreply.github.com>
2024-09-05 20:13:36 +0100

    test delete album service layer

--- src/test/java/com/northcoders/record_shop/service/delete/TestDeleteAlbumService.java
+++ src/test/java/com/northcoders/record_shop/service/delete/TestDeleteAlbumService.java
@@ -1,4 +1,31 @@
 package com.northcoders.record_shop.service.delete;
 
+import com.northcoders.record_shop.repository.AlbumRepository;
+import com.northcoders.record_shop.service.AlbumService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.mockito.Mockito.*;
+
+@DataJpaTest
 public class TestDeleteAlbumService {
+
+    @Mock
+    private AlbumRepository mockAlbumRepository;
+
+    @InjectMocks
+    private AlbumService albumService;
+
+    @Test
+    @DisplayName("Delete an album")
+    void deleteAlbumTest() throws Exception{
+        Long id = 1L;
+        when(mockAlbumRepository.existsById(id)).thenReturn(true);
+        albumService.deleteAlbum(id);
+        verify(mockAlbumRepository, times(1)).deleteById(id);
+    }
 }
+
