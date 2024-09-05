4fb93163af082bbb50dd45e356e5d0502ba2cb2b
dMarsh <125702046+Alan-Turing13@users.noreply.github.com>
2024-09-05 11:39:07 +0100

    pass get all service test

--- src/test/java/com/northcoders/record_shop/TestService.java
+++ src/test/java/com/northcoders/record_shop/TestService.java
@@ -1,13 +1,46 @@
 package com.northcoders.record_shop;
 
+import com.northcoders.record_shop.model.Album;
+import com.northcoders.record_shop.model.Genre;
 import com.northcoders.record_shop.repository.AlbumRepository;
+import com.northcoders.record_shop.service.AlbumService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 public class TestService {
 
     @Mock
     private AlbumRepository mockAlbumRepository;
+
+    @InjectMocks
+    private AlbumService albumService;
+
+    @Test
+    @DisplayName("get all albums")
+    void getAllAlbumsTest() throws Exception{
+        List<Album> mockAlbums = List.of(new Album(
+                1L,
+                "Criss-Cross",
+                1963,
+                Genre.JAZZ,
+                "Thelonious Monk"
+        ));
+
+        when(mockAlbumRepository.findAll()).thenReturn(mockAlbums);
+
+        List<Album> serviceResult = albumService.getAllAlbums();
+
+        assertIterableEquals(serviceResult, mockAlbums);
+
+    }
 
 }
