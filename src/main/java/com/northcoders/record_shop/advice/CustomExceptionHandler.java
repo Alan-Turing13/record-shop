7e108f37420e3625e6b92cab5ccb8c697c1dc187
dMarsh <125702046+Alan-Turing13@users.noreply.github.com>
2024-09-10 18:23:32 +0100

    update readme

--- src/main/java/com/northcoders/record_shop/advice/CustomExceptionHandler.java
+++ src/main/java/com/northcoders/record_shop/advice/CustomExceptionHandler.java
@@ -20,38 +20,4 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(iie.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public Map<String, String> handleInvalidInput(MethodArgumentNotValidException exception){
-//        Map<String, String> errorMessage = new HashMap<>();
-//        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
-//            errorMessage.put(fieldError.getField(), fieldError.getDefaultMessage());
-//        });
-//        return errorMessage;
-//    }
-//
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(HttpMessageNotReadableException.class)
-//    public Map<String, String> handleInvalidGenre(HttpMessageNotReadableException exception){
-//        Map<String, String> errorMessage = new HashMap<>();
-//        errorMessage.put("Error", exception.getMessage());
-//        return errorMessage;
-//    }
-//
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler(NotFoundException.class)
-//    public Map<String, String> handleNotFound(NotFoundException e){
-//        Map<String, String> errorMessage = new HashMap<>();
-//        errorMessage.put("Error", e.getMessage());
-//        return errorMessage;
-//    }
-//
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler(TemplateInputException.class)
-//    public Map<String, String> templateInput(TemplateInputException e){
-//        Map<String, String> errorMessage = new HashMap<>();
-//        errorMessage.put("Error", e.getMessage());
-//        return errorMessage;
-//    }
-
 }
